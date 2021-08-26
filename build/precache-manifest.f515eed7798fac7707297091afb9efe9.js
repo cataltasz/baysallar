bcec3184182c522b874ebe73c62ae0e5f28a6c8f
cataltasz <mustafacataltas@yandex.com>
2021-08-26 19:26:54 +0300

    homepage

---
+++ build/precache-manifest.f515eed7798fac7707297091afb9efe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caddb67197254a03164dc7bbadd04875",
+    "url": "/baysallar/index.html"
+  },
+  {
+    "revision": "5233c476832ec534ed93",
+    "url": "/baysallar/static/css/main.214c8dd5.chunk.css"
+  },
+  {
+    "revision": "c001bffce2d930212159",
+    "url": "/baysallar/static/js/2.d1b8d25e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/baysallar/static/js/2.d1b8d25e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5233c476832ec534ed93",
+    "url": "/baysallar/static/js/main.19f39af7.chunk.js"
+  },
+  {
+    "revision": "9bed5a175cf10aa3a93f",
+    "url": "/baysallar/static/js/runtime-main.53a69c99.js"
+  }
+]);
