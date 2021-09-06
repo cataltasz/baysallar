03a6c4a75c6dc2fb5bd463d627471cc87879b23c
cataltasz <mustafacataltas@yandex.com>
2021-09-06 11:49:42 +0300

    Updates

--- precache-manifest.7f84236d9a2b7cc28d00ba4bcfb52eb2.js
+++ precache-manifest.7f84236d9a2b7cc28d00ba4bcfb52eb2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1096acfa4dfb5478cd275ecc89e11425",
+    "revision": "0a106f30966850ae0bd55b6ca329bc4d",
     "url": "/baysallar/index.html"
   },
   {
-    "revision": "13bc800425635377a864",
+    "revision": "0f1173a7ea6cf03a50c7",
     "url": "/baysallar/static/css/main.949b17f7.chunk.css"
   },
   {
-    "revision": "8d1b82f2233a3e2f1846",
-    "url": "/baysallar/static/js/2.9d6c9657.chunk.js"
+    "revision": "462c3f04217e99a9e8a2",
+    "url": "/baysallar/static/js/2.dcf0bb77.chunk.js"
   },
   {
     "revision": "9b9de423b52b798b8fc649e7d9e276fb",
-    "url": "/baysallar/static/js/2.9d6c9657.chunk.js.LICENSE.txt"
+    "url": "/baysallar/static/js/2.dcf0bb77.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "13bc800425635377a864",
-    "url": "/baysallar/static/js/main.8d1c80ca.chunk.js"
+    "revision": "0f1173a7ea6cf03a50c7",
+    "url": "/baysallar/static/js/main.aca6a975.chunk.js"
   },
   {
     "revision": "9bed5a175cf10aa3a93f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/baysallar/precache-manifest.a2f78fb1ef94f176793b7e0ce61bdd4f.js"
+  "/baysallar/precache-manifest.7f84236d9a2b7cc28d00ba4bcfb52eb2.js"
 );
 
 self.addEventListener('message', (event) => {
